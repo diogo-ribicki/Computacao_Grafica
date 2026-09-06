6d395d8cfdfc7fa3139fc6349750f03ab607f936
pedrxbro <pedrohdpaula@edu.univali.br>
2026-09-06 20:25:05 -0300

    feat: cria a estrutura do cubo

---
+++ ComputacaoGrafica/src/Cubo.cpp
@@ -0,0 +1,54 @@
+#include "Cubo.hpp"
+
+Cubo criar_cubo(
+    double posicao_x,
+    double posicao_y,
+    double posicao_z,
+    double tamanho_lado
+) {
+    Cubo novo_cubo;
+
+    novo_cubo.tamanhoLado = tamanho_lado;
+
+    novo_cubo.posicao = {
+        posicao_x,
+        posicao_y,
+        posicao_z
+    };
+
+    double metade = tamanho_lado / 2.0;
+
+    novo_cubo.vertices = {
+        { posicao_x - metade, posicao_y - metade, posicao_z + metade }, // 0
+        { posicao_x + metade, posicao_y - metade, posicao_z + metade }, // 1
+        { posicao_x + metade, posicao_y + metade, posicao_z + metade }, // 2
+        { posicao_x - metade, posicao_y + metade, posicao_z + metade }, // 3
+
+        { posicao_x - metade, posicao_y - metade, posicao_z - metade }, // 4
+        { posicao_x + metade, posicao_y - metade, posicao_z - metade }, // 5
+        { posicao_x + metade, posicao_y + metade, posicao_z - metade }, // 6
+        { posicao_x - metade, posicao_y + metade, posicao_z - metade }  // 7
+    };
+
+    novo_cubo.arestas = {
+        // Frente
+        {0, 1},
+        {1, 2},
+        {2, 3},
+        {3, 0},
+
+        // Tras
+        {4, 5},
+        {5, 6},
+        {6, 7},
+        {7, 4},
+
+        // Ligacoes entre frente e tras
+        {0, 4},
+        {1, 5},
+        {2, 6},
+        {3, 7}
+    };
+
+    return novo_cubo;
+}
